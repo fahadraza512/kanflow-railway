b05bd3a1030f5aa2836f27194cf4c25fc9c7a566
Kanban Developer <dev@kanban.local>
2026-03-17 13:58:53 +0500

    fix: move toast position to bottom-right

--- frontend/src/providers/ToastProvider.tsx
+++ frontend/src/providers/ToastProvider.tsx
@@ -5,7 +5,7 @@ import { Toaster } from 'react-hot-toast';
 export default function ToastProvider() {
     return (
         <Toaster
-            position="top-right"
+            position="bottom-right"
             reverseOrder={false}
             gutter={8}
             toastOptions={{
